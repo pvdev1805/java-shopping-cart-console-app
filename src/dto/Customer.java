0af936796688bbc99d26b98146366c1bafabd7a4
Phu Vo <u1153426@umail.usq.edu.au>
2025-05-14 16:45:32 +1000

    :adhesive_bandage: fix: Remove cartId in Customer class

--- src/dto/Customer.java
+++ src/dto/Customer.java
@@ -5,14 +5,12 @@ public class Customer {
 	private String name;
 	private String accountId;
 	private String rankId;
-	private String cartId;
 	
-	public Customer(String customerId, String name, String accountId, String rankId, String cartId) {
+	public Customer(String customerId, String name, String accountId, String rankId) {
 		this.customerId = customerId;
 		this.name = name;
 		this.accountId = accountId;
 		this.rankId = rankId;
-		this.cartId = cartId;
 	}
 	
 	public String getCustomerId() {
@@ -25,10 +23,6 @@ public class Customer {
 	
 	public String getRankId() {
 		return rankId;
-	}
-	
-	public String getCartId() {
-		return cartId;
 	}
 	
 	public String getAccountId() {
